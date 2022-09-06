19e80156aba1ab682588b5c827e2a0052331f655
Victor <victor.fsilva45@gmail.com>
2022-09-06 11:46:51 -0300

    Estrutura

--- api.php
+++ api.php
@@ -1,3 +1,32 @@
 <?php 
-// Api - Aplicação para recursos de app mobile
+    // Api - Aplicação para recursos de app mobile
+    include_once('conn.php');
+
+    // Variável que recebe o conteúdo da requisição do APP decodificando-a (json)
+    $postjson = json_decode(file_get_contents('php://input',true),true);
+
+if ($postjson['requisicao']=='add') {
+    $query = $pdo->prepare("insert into usuarios set nome= :nome, usuario=:usuario, senha=:senha, senha_original=:senha_original, nivel=:nivel, ativo=1");
+    $query->bindValue(":nome", $postjson['nome']);
+    $query->bindValue(":usuario", $postjson['usuario']);
+    $query->bindValue(":senha", md5($postjson['senha']));
+    $query->bindValue(":senha_original", $postjson['senha']);
+    $query->bindValue(":nivel", $postjson['nivel']);
+    $query->execute();
+    $id = $pdo->lastInsertId();
+
+    if ($query) {
+        $result = json_encode(array('success'=>true,'id'=>$id));
+    }else{
+        $result = json_encode(array('success'=>false,'msg'=>'Falha ao inserir o usuário'));
+    }
+    echo $result;
+}// Final requisição add
+else if ($postjson['requisicao']=='listar') {
+    if($postjson['nome']==''){
+        $query= $pdo->query("SELECT * FROM usuarios order BY id desc limit $postjson['start'], $postjson[limit]");
+    }else{
+
+    }
+}
 ?>
--- conn.php
+++ conn.php
@@ -1,3 +1,31 @@
 <?php 
 // Conexão com banco de dados da app mobile ionic
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Crendentials: true');
+header('Access-Control-Methods: POST, GET, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Request-With');
+header('Content-Type: application/json; charset=utf-8');
+
+// Dados do servidor local
+$banco = 'controledb';
+$host = 'localhost'; // 127.0.0.1
+$usuario = 'root';
+$senha = '';
+
+/*
+// Dados do servidor remoto/hospedado
+$banco = 'wellingtom_91';
+$host = 'softkleen.com.br'; // 127.0.0.1
+$usuario = 'apiti9111';
+$senha = '';
+*/
+
+try{
+    $pdo = new PDO("mysql:dbname=$banco;host=$host","$usuario", "$senha");
+} catch (Exception $e){
+    echo 'Erro ao conectar com o banco!';
+}
+
+
+
 ?>
