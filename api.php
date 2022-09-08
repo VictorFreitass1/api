4130381de31d6e26688eb2739878ffbbae60a425
Victor <victor.fsilva45@gmail.com>
2022-09-08 11:46:15 -0300

    LOGIN USUARIOS

--- api.php
+++ api.php
@@ -24,9 +24,85 @@ if ($postjson['requisicao']=='add') {
 }// Final requisição add
 else if ($postjson['requisicao']=='listar') {
     if($postjson['nome']==''){
-        $query= $pdo->query("SELECT * FROM usuarios order BY id desc limit $postjson['start'], $postjson[limit]");
+        $query = $pdo->query("SELECT * FROM usuarios order BY id desc limit " .$postjson['start']. "," .$postjson['limit']);
     }else{
-
+        $busca = '%'.$postjson['nome'].'%';
+        $query = $pdo->query("SELECT * FROM usuarios WHERE nome LIKE '$busca' or usuario LIKE '$busca' order BY id desc limit " .$postjson['start']. "," .$postjson ['limit']);
     }
+    $res = $query->fetchAll(PDO::FETCH_ASSOC);
+    for($i=0;$i < count($res);$i++){
+        $dados [] = array(
+            'id'                =>$res[$i]['id'],
+            'nome'              =>$res[$i]['nome'],
+            'usuario'           =>$res[$i]['usuario'],
+            'senha'             =>$res[$i]['senha'],
+            'senha_original'    =>$res[$i]['senha_original'],
+            'nivel'             =>$res[$i]['nivel'],
+            'ativo' =>$res[$i]['ativo']
+        );
+    } 
+    if(count($res)>0){
+        $result = json_encode(array('success'=>true, 'result'=>$res));
+    }
+    else{
+        $result = json_encode(array('success'=>false, 'result'=>'Eita Cláudia....'));
+    }
+    echo ($result);
+}// Fim do listar
+else if($postjson['requisicao']=='editar'){
+    $query= $pdo->prepare("UPDATE usuarios SET nome=:nome, usuario=:usuario, senha=:senha, senha_original=:senha_original, nivel=:nivel WHERE id=:id");
+    $query->bindValue(":nome",$postjson['nome']);
+    $query->bindValue(":usuario",$postjson['usuario']);
+    $query->bindValue(":senha",md5($postjson['senha']));
+    $query->bindValue(":senha_original",$postjson['senha']);
+    $query->bindValue(":nivel",$postjson['nivel']);
+    $query->bindValue(":id",$postjson['id']);
+    $query->execute();
+    if ($query){
+        $result = json_encode(array('success'=>true, 'msg' =>'Deu tudo certo com a alteração!'));
+    } else {
+        $result = json_encode(array('success'=>false,'msg' =>'Dados incorretos! Falha ao atualizar o usuário! (WRH014587)'));
+    }
+    echo $result;
+} // Final da requisição editar
+    else if ($postjson['requisicao']=='excluir'){
+        $query = $pdo->query("Delete from usuarios where id = $postjson[id]");
+    //  $query = $pdo->query("update usuarios set ativo = 0 where id $postjson[id]");
+    if ($query){
+        $result = json_encode(array('success'=>true, 'msg' =>'Usuário excluído com sucesso!'));
+    } else {
+        $result = json_encode(array('success'=>false,'msg' =>'Falha ao excluir o usuário!'));
+    }
+    echo $result;
+} // Final do excluir
+    else if ($postjson['requisicao']=='login'){
+        $query = $pdo->query("SELECT * from usuarios where usuario = '$postjson[usuario]' and senha = md5('$postjson[senha]') and ativo = 1");
+
+        $res = $query->fetchAll(PDO::FETCH_ASSOC);
+        for($i=0;$i < count($res);$i++){
+            $dados [] = array(
+                'id'                =>$res[$i]['id'],
+                'nome'              =>$res[$i]['nome'],
+                'usuario'           =>$res[$i]['usuario'],
+                'nivel'             =>$res[$i]['nivel'],
+                'ativo' =>          $res[$i]['ativo']
+            );
+    }
+
+    if ($query){
+        $result = json_encode(array('success'=>true, 'result'=>$dados));
+    } else {
+        $result = json_encode(array('success'=>false,'msg' =>'Falha ao efetuar login!'));
+    }
+    echo $result;
+}// Final do login
+else if ($postjson['requisicao']=='ativar'){
+        $query = $pdo->query("UPDATE usuarios set ativo = 1 where id = $postjson[id]");
+    if ($query){
+        $result = json_encode(array('success'=>true, 'msg' =>'Usuário ativado com sucesso!'));
+    } else {
+        $result = json_encode(array('success'=>false,'msg' =>'Falha ao ativar o usuário!'));
+    }
+    echo $result;
 }
 ?>
